3afb01f5df2814a382df9f1d3018133b05a6b885
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-08-05 20:20:14 +0000

    edit arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,10 +4,27 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a,b) {
+//     //code block
+//     return a + b
+// }
+// let sum = addTwoNumbers(3, 5)
+// console.log(sum)
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    //code block
+    return a + b
+}
+let sum = addTwoNumbers(3, 5)
+console.log(sum)
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a, b) => (a + b); also valid
+const addTwoNumbers2 = (a, b) => a + b;
+
+let sum2 = addTwoNumbers2(8, 10);
+console.log(sum2);
 
 // Implicit Returns
 
